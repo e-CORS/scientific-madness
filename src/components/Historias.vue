4e41f2fa47d38b7d5ae39b68ba005bbc41358af8
Enzo Corrales <enzo.corrales@sandbox.game>
2023-10-07 02:22:41 -0300

    add: 2 more pages

--- src/components/Historias.vue
+++ src/components/Historias.vue
@@ -1,10 +1,64 @@
 <template>
-  <div>
-    <p>PAGE IS IN PROGRESS</p>
+  <div class="history-wrapper">
+    <div
+      v-for="(historyEntry, index) in history"
+      :key="`history-${index}`"
+      class="history-entry"
+    >
+      <h3 class="entry-title">{{ historyEntry.title }}</h3>
+      <br />
+      <em class="entry-text">
+        {{ historyEntry.text }}
+      </em>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
 
-<style scoped></style>
-ß
+const history = ref([
+  {
+    title: "El Baile Incontrolable:",
+    text: "Libere una lluvia constante de confeti en un evento al aire libre, creando un caos de papel brillante que cubría todo y dejaba a las personas asombradas y divertidas, pero también completamente cubiertas de confeti.",
+  },
+  {
+    title: "El Día de la Lluvia de Confeti:",
+    text: "Mi última creación, la 'Máquina de Distorsión Musical', ha tenido un gran éxito esta noche. He reprogramado todos los sistemas de sonido de la fiesta para que reproduzcan música clásica a velocidad extrema y al revés. Las caras de horror y desconcierto de los invitados valieron la pena cada hora de trabajo que invertí en ella.",
+  },
+  {
+    title: "La Conferencia de Risas:",
+    text: "Interrumpi una conferencia científica seria con su gas de la risotada, haciendo que los asistentes, incluidos los científicos más respetados, no pudieran dejar de reírse durante horas. La conferencia se convirtió en una hilarante reunión que fue imposible de tomar en serio.",
+  },
+  {
+    title: "La Fiesta del Espejismo:",
+    text: "Organice una fiesta en un lugar que solo existía como un holograma, haciendo que los invitados se encontraran bailando en un lugar que, en realidad, no estaba ahí. Esta experiencia surrealista dejó a todos preguntándose si habían estado en la fiesta en primer lugar.",
+  },
+  {
+    title: "La Carrera de Confusión::",
+    text: "Organice una carrera de obstáculos en la que los obstáculos eran ilusiones ópticas creadas por su proyector de hologramas. Los participantes se encontraron corriendo hacia paredes que no existían y saltando obstáculos que desaparecían, creando una escena de total desconcierto y diversión.",
+  },
+  {
+    title: "El Desfile de Sombras:",
+    text: "Organice un desfile de sombras gigantes en la noche, proyectando siluetas enormes y extrañas en los edificios de la ciudad. Esto creó una atmósfera surrealista y fantasmagórica que dejó a la ciudad entera preguntándose si estaban experimentando un evento sobrenatural.",
+  },
+]);
+</script>
+
+<style scoped>
+.history-wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.history-entry {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  text-align: left;
+  margin-bottom: 50px;
+}
+</style>
--- src/components/Tips.vue
+++ src/components/Tips.vue
@@ -1,9 +1,57 @@
 <template>
-  <div>
-    <p>PAGE IS IN PROGRESS</p>
+  <div class="tips-wrapper">
+    <div
+      v-for="(tipsEntry, index) in tips"
+      :key="`tip-${index}`"
+      class="tips-entry"
+    >
+      <em class="entry-date">Tip: {{ index + 1 }}</em>
+      <br />
+      <em class="entry-text">
+        {{ tipsEntry.text }}
+      </em>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const tips = ref([
+  {
+    text: "La fiesta se llevó a cabo en una pequeña sala de estar, donde la iluminación era tenue y las paredes estaban adornadas con un papel pintado anticuado y descolorido. La música de fondo era una melodía monótona que apenas se podía escuchar, y nadie se atrevía a acercarse al reproductor para cambiarla.",
+  },
+  {
+    text: "Los invitados, que parecían haber sido arrastrados a la fiesta por obligación en lugar de entusiasmo, estaban sentados en sofás desgastados y sillas incómodas. Conversaban en susurros, pero sus temas de conversación eran tan insípidos como la decoración: el clima, los informes de tráfico y otros temas igualmente mundanos.",
+  },
+  {
+    text: "La comida consistía en una selección de bocadillos secos y aburridos: galletas saladas, pretzels y una bandeja de zanahorias y apio que nadie tocaba. Las bebidas eran refrescos sin gas que habían perdido su efervescencia hace mucho tiempo.",
+  },
+  {
+    text: "No había actividades ni entretenimiento en la fiesta. No se veían juegos ni concursos, ni siquiera una simple mesa de billar o cartas. La ausencia de cualquier tipo de diversión hacía que el tiempo pasara lentamente.",
+  },
+  {
+    text: "Los anfitriones apenas hacían acto de presencia y, cuando lo hacían, parecían igual de aburridos que sus invitados. No se percibía emoción ni energía en la sala, y las expresiones de todos los presentes reflejaban claramente su deseo de estar en cualquier otro lugar.",
+  },
+  {
+    text: "En resumen, esta fiesta era un ejemplo perfecto de aburrimiento absoluto, donde la falta de ambiente, entretenimiento y conversaciones interesantes la convertía en una experiencia que nadie quería repetir.",
+  },
+]);
+</script>
 
-<style scoped></style>
+<style scoped>
+.tips-wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.tips-entry {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  text-align: left;
+  margin-bottom: 50px;
+}
+</style>
